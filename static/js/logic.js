53796c6e0814919c3ad06a1a46c035254b7c5b68
Chad Milburn <cmilburn@ballstate.bsu.edu>
2021-04-15 23:16:57 -0400

    all working but color on legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,6 @@
 //// Store API inside queryUrl -using hour data for inital build
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var tectonicPlatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 //   preform request on url and retrieve features from GEOJson
 d3.json(queryUrl, function (data) {
   createFeatures(data.features);
@@ -10,15 +11,16 @@ function createFeatures(earthquakeData) {
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: function (feature, layer) {
       layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>")
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p>" + "Depth: " + feature.geometry['coordinates'][2] + "</p>" + "<p>" + "Mag : " + feature.properties.mag + "</p>")
     },
     pointToLayer: function (feature, latlng) {
       return new L.circle(latlng,
+
         {
           radius: radiusSize(feature.properties.mag),
-          fillColor: circleColor(feature.properties.mag),
+          fillColor: getColor(feature.geometry['coordinates'][2]),
           fillOpacity: .75,
-          color: "",
+          color: "white",
         })
     }
   });
@@ -54,28 +56,53 @@ function createMap(earthquakes) {
     "Dark Map": darkmap,
     "Satellite Map": satmap
   };
-  // -----------------------------------------------------------------------------------
-  // -----------------------------------------------------------------------------------
+  var tectonicPlates = new L.LayerGroup()
   //  create overlay object
   var overlayMaps = {
-    "Earthquakes": earthquakes
-    // -----------------------
-    // -----------------------
-  };
+    "Earthquakes": earthquakes,
+    "Techtonic Plates": tectonicPlates
+   };
   // Create our map, giving it the layers to display on load
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [outdoorsmap, earthquakes]
+    layers: [outdoorsmap, earthquakes,tectonicPlates]
   });
+  d3.json(tectonicPlatesURL, function(tData){
+    L.geoJSON(tData, {
+      color: "blue",
+      weight: 2
+    })
+    .addTo(tectonicPlates)
+  })
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
   // Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+  // create legend https://gis.stackexchange.com/questions/193161/add-legend-to-leaflet-map or https://leafletjs.com/examples/choropleth/
+  var legend = L.control({
+    position: "bottomleft"
+  })
+
+  legend.onAdd = function () {
+    var div = L.DomUtil.create('div', 'info legend'),
+      grades = [0, 5, 10, 15, 20],
+      labels = []
+
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        '<i style=background:' + getColor(grades[i] + 1) + '"></i>' + grades[i] +(grades[i+1] ? '&ndash;' + grades[i+1]+'<br>' : '+')
+    }
+    return div
+  }
+  legend.addTo(myMap)
+
+
 }
 
 
@@ -83,27 +110,46 @@ function createMap(earthquakes) {
 function radiusSize(mag) {
   return mag * 50000
 }
-// function circleColor(mag){
-//   if (mag<1){
-//     return "#e6f5ff"
-//   }
-//   else if (mag<2){
-//     return "#b3e0ff"
-//   } 
-//   else if (mag<3){
-//     return "#80ccff"
-//   }
-//   else if (mag<4){
-//     return "#4db8ff"
-//   } else {
-//     return "#1aa3ff"
-//   }
-// }
-function circleColor(d) {
-  return d > 1 ? '#800026' :
-        d > 2 ? '#BD0026' :
-        d > 3 ? '#E31A1C' :
-        d > 4 ? '#FC4E2A' :
-        d > 5 ? '#FD8D3C' :
-        '#FFEDA0';
+function getColor(d) {
+  if (d > 20) {
+    return "red"
+  }
+  else if (d > 15) {
+    return "orange"
+  }
+  else if (d > 10) {
+    return "yellow"
+  }
+  else if (d > 5) {
+    return "blue"
+  } else {
+    return "magenta"
+  }
 }
+// function circleColor(d) {
+//   return d <  ? 'red' :
+//         d > 2 ? 'green' :
+//         d > 3 ? 'yellow' :
+//         d > 4 ? 'blue' :
+//         d > 5 ? 'black' :
+//         'purple';
+// }
+
+// function radiusSize(mag) {
+//   return mag * 50000
+// }
+// function circleColor(d) {
+//   if (d < 1) {
+//     return "#fce2fb"
+//   }
+//   else if (d < 5 & d > 1 ) {
+//     return "#fef5ee"
+//   }
+//   else if (d < 10 & d > 5) {
+//     return "#932598"
+//   }
+//   else if (d > 10) {
+//     return "#c91367"
+//   } else {
+//     return "#380a8a"
+//   }
